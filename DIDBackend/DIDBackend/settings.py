6d6114a27992a43d88e47dc9e1f4ad3d2b64eefc
Dipen patel <pdipen135@gmail.com>
2024-10-31 03:21:37 +0530

    env os setup

--- DIDBackend/DIDBackend/settings.py
+++ DIDBackend/DIDBackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -176,9 +177,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'emailhelper468@gmail.com' 
-EMAIL_HOST_PASSWORD = 'jadf dtgc uths mhqt' 
 
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 
