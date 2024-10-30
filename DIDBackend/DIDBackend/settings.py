5b7155dd9aa91ad8894a4578f1de50b0f349c565
Dipen patel <pdipen135@gmail.com>
2024-10-31 02:39:51 +0530

    static files

--- DIDBackend/DIDBackend/settings.py
+++ DIDBackend/DIDBackend/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
