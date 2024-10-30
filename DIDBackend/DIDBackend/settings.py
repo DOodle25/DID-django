e8555b592e7fb9d315a3bfe299f4ef5fcd3badc4
Dipen patel <pdipen135@gmail.com>
2024-10-31 01:09:49 +0530

    params

--- DIDBackend/DIDBackend/settings.py
+++ DIDBackend/DIDBackend/settings.py
@@ -163,8 +163,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'setup yours' 
-EMAIL_HOST_PASSWORD = 'setup yours' 
+EMAIL_HOST_USER = 'emailhelper468@gmail.com' 
+EMAIL_HOST_PASSWORD = 'jadf dtgc uths mhqt' 
 
 
 
